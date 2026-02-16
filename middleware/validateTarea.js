e9f2d40cc64dda9279a3fb8ffa6b47d3746ad33f
mauricioglpz <Al07023486@tecmilenio.mx>
2026-02-15 21:26:18 -0600

    Delete middleware directory

--- middleware/validateTarea.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    const { titulo, descripcion } = req.body;
-
-    if (!titulo || !descripcion) {
-        return res.status(400).json({ mensaje: 'Título y descripción obligatorios' });
-    }
-
-    next();
-};
